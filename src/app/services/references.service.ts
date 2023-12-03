ab0e799fa15f8d01cd77b946bc9d1d0b71ecb9ad
Stefan Mitterlehner <stefan-mitterlehner@outlook.com>
2023-12-03 20:00:31 +0100

    content update

--- src/app/services/references.service.ts
+++ src/app/services/references.service.ts
@@ -2,12 +2,24 @@ import { Injectable } from '@angular/core';
 import { HeroListItem } from '../models/hero-list-item.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReferencesService {
-
   getReferences(): HeroListItem[] {
     return [
+      {
+        title: 'Time Tracker for VS Code',
+        description:
+          'Track work time within the repository, directly from VS Code',
+        link: 'https://marketplace.visualstudio.com/items?itemName=S-Mitterlehner.time-tracker-vscode',
+        imagePath: 'assets/references/time-tracker-vscode.png',
+        tech: [
+          {
+            name: 'TypeScript',
+            icon: 'assets/tech/ts.svg',
+          },
+        ],
+      },
       {
         title: 'Website: Trafik Mitterlehner',
         description: 'Online presence for "Trafik Mitterlehner"',
@@ -16,53 +28,55 @@ export class ReferencesService {
         tech: [
           {
             name: 'Angular',
-            icon: 'assets/tech/angular.svg'
-          }
-        ]
+            icon: 'assets/tech/angular.svg',
+          },
+        ],
       },
       {
         title: 'Product Information System',
-        description: 'Many extensions of the product information system of a big austrian company, including a mass-change tool and a generic exporter',
+        description:
+          'Many extensions of the product information system of a big austrian company, including a mass-change tool and a generic exporter',
         imagePath: 'assets/references/placeholder.png',
         link: undefined,
         tech: [
           {
             name: 'C#',
-            icon: 'assets/tech/csharp.svg'
+            icon: 'assets/tech/csharp.svg',
           },
           {
             name: 'WPF',
-            icon: 'assets/tech/wpf.png'
+            icon: 'assets/tech/wpf.png',
           },
           {
             name: 'Microsoft SQL',
-            icon: 'assets/tech/mssql.svg'
-          }
-        ]
+            icon: 'assets/tech/mssql.svg',
+          },
+        ],
       },
       {
         title: 'Phonebook',
-        description: 'Internal Phonebook for a big austrian company, including employee-, store-, department data and translation.',
+        description:
+          'Internal Phonebook for a big austrian company, including employee-, store-, department data and translation.',
         imagePath: 'assets/references/placeholder.png',
         link: undefined,
         tech: [
           {
             name: 'Angular',
-            icon: 'assets/tech/angular.svg'
+            icon: 'assets/tech/angular.svg',
           },
           {
             name: 'C#',
-            icon: 'assets/tech/csharp.svg'
+            icon: 'assets/tech/csharp.svg',
           },
           {
             name: 'ASP.NET',
-            icon: 'assets/tech/aspnet.svg'
+            icon: 'assets/tech/aspnet.svg',
           },
           {
             name: 'Microsoft SQL',
-            icon: 'assets/tech/mssql.svg'
-          }
-        ]
+            icon: 'assets/tech/mssql.svg',
+          },
+        ],
       },
       {
         title: 'My Portfolio',
@@ -72,9 +86,9 @@ export class ReferencesService {
         tech: [
           {
             name: 'Angular',
-            icon: 'assets/tech/angular.svg'
-          }
-        ]
+            icon: 'assets/tech/angular.svg',
+          },
+        ],
       },
       {
         title: 'In Memoriam: Christian Mitterlehner',
@@ -84,18 +98,18 @@ export class ReferencesService {
         tech: [
           {
             name: 'HTML',
-            icon: 'assets/tech/html.svg'
+            icon: 'assets/tech/html.svg',
           },
           {
             name: 'CSS',
-            icon: 'assets/tech/css.svg'
+            icon: 'assets/tech/css.svg',
           },
           {
             name: 'JavaScript',
-            icon: 'assets/tech/js.svg'
-          }
-        ]
-      }
-    ]
+            icon: 'assets/tech/js.svg',
+          },
+        ],
+      },
+    ];
   }
 }
