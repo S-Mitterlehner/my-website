b03054b8a9b68deb61321f64700ddeb9c8b88f38
Stefan Mitterlehner <stefan-mitterlehner@outlook.com>
2026-07-08 10:24:53 +0200

    updates 2026

--- src/app/models/chronological-list-item.model.ts
+++ src/app/models/chronological-list-item.model.ts
@@ -1,13 +1,17 @@
 export interface ChronologicalListItem {
-    from: string;
-    to: string | undefined;
-    primaryText: string;
-    secondaryText: string | undefined;
-    icon: string;
-    size: string;
-    details: ChronologicalDetails | undefined;
+  from: string;
+  to: string | undefined;
+  primaryText: string;
+  secondaryText: string | undefined;
+  icon: string;
+  size: string;
+  details: ChronologicalDetails | undefined;
 }
 
 export interface ChronologicalDetails {
-    tasks: string[];
+  thesis?: {
+    name: string;
+    description: string;
+  };
+  tasks?: string[];
 }
